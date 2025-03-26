b3298f5e02ff3e787830bbb0af31ce7918c0656f
GUSTAVO DE PAULA SILVA <25132581@CFP127ALUNO.LOCAL>
2025-03-26 14:09:01 -0300

    segundo commit

--- src/br/dev/gustavo/git_test/AppTest.java
+++ src/br/dev/gustavo/git_test/AppTest.java
@@ -3,7 +3,7 @@ package br.dev.gustavo.git_test;
 public class AppTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Olá mundo!");
 
 	}
 
