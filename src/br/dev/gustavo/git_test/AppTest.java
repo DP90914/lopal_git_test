822bf5c5b21812b76a4ed9685500030702fd5679
DP90914 <gustavodepaulasilva299@gmail.com>
2025-03-26 14:14:47 -0300

    Atualizei o arquivo AppTest.java

--- src/br/dev/gustavo/git_test/AppTest.java
+++ src/br/dev/gustavo/git_test/AppTest.java
@@ -4,7 +4,8 @@ public class AppTest {
 
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
-
+		System.out.println("senai jandira");
+		System.out.println("turma ds1t-a");
 	}
 
 }
