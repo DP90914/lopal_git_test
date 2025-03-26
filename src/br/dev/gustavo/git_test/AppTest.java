2247e1a9f93ee8442679747f6308fe05a5adf5e2
GUSTAVO DE PAULA SILVA <25132581@CFP127ALUNO.LOCAL>
2025-03-26 14:24:36 -0300

    quarto commit do local

--- src/br/dev/gustavo/git_test/AppTest.java
+++ src/br/dev/gustavo/git_test/AppTest.java
@@ -3,8 +3,6 @@ package br.dev.gustavo.git_test;
 public class AppTest {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
-		System.out.println("senai jandira");
 		System.out.println("turma ds1t-a");
 		System.out.println("turma ds1t-a");
 		System.out.println("turma ds1t-a");
